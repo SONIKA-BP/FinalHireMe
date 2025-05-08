03f12f305f73ca87fa0f52a3a5fed6d6ea910530
Sonika B P <sonikabpsonikabp@gmail.com>
2025-05-08 19:35:20 +0530

    my first commit

--- app.py
+++ app.py
@@ -196,3 +196,5 @@ def api_report_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # port = int(os.environ.get("PORT", 5000))  
+    # app.run(host="0.0.0.0", port=port,debug=True)
