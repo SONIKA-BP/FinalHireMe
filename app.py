4ad08bd82de4965275d64ac3fbf0d6d900296381
Sonika B P <sonikabpsonikabp@gmail.com>
2025-05-09 14:30:53 +0530

    lastFinal

--- app.py
+++ app.py
@@ -196,10 +196,6 @@ def api_report_data():
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 10000))  
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port,debug=True)
 
-
-# port = int(os.environ.get("PORT", 10000))
-# app.run(host='0.0.0.0', port=port)
